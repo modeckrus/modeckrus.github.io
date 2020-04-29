9b500e019b14188ad918efd0d5cfa39cb505bfec
modeckrus <modeckrus.official.page@gmail.com>
2020-04-30 00:32:58 +0300

    fix iceServers

--- js/plainwebrtc.js
+++ js/plainwebrtc.js
@@ -1,4 +1,4 @@
-var conf = {iceServers: [{urls: [{'url': 'stun:stun.l.google.com:19302'}]}]};
+var conf = {iceServers: [{urls: ['stun:stun.l.google.com:19302']}]};
 var pc = new RTCPeerConnection(conf);
 var localStream, _fileChannel, chatEnabled,context,source,
 	_chatChannel,sendFileDom = {}, 
